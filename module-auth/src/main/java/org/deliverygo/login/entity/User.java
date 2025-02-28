170ae1f3674d979ef2d9ce64ed8c3637734fe354
js.min <js.min@hansol.com>
2025-02-28 19:08:04 +0900

    update: user 엔티티에 equals & hashCode 추가

--- module-auth/src/main/java/org/deliverygo/login/entity/User.java
+++ module-auth/src/main/java/org/deliverygo/login/entity/User.java
@@ -7,6 +7,8 @@ import org.deliverygo.login.constants.UserGrade;
 import org.deliverygo.login.dto.SignUpRequest;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import java.util.Objects;
+
 @Entity
 @Getter
 @NoArgsConstructor
@@ -48,5 +50,23 @@ public class User extends BaseEntity {
 
     private static String encryptPassword(PasswordEncoder passwordEncoder, String password) {
         return passwordEncoder.encode(password);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return getPhone() == user.getPhone() &&
+                Objects.equals(getId(), user.getId()) &&
+                Objects.equals(getPassword(), user.getPassword()) &&
+                Objects.equals(getName(), user.getName()) &&
+                Objects.equals(getEmail(), user.getEmail()) &&
+                Objects.equals(getAddress(), user.getAddress()) &&
+                getGrade() == user.getGrade();
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getPassword(), getName(), getEmail(), getAddress(), getPhone(), getGrade());
     }
 }
