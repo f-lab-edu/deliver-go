e3d4d4ddb508084802a4ba2192f1ae25218bc99b
js.min <js.min@hansol.com>
2025-03-01 17:26:17 +0900

    add: 회원가입 테스트코드 추가

---
+++ module-auth/src/test/java/org/deliverygo/login/service/SignUpServiceImplTest.java
@@ -0,0 +1,50 @@
+package org.deliverygo.login.service;
+
+import jakarta.transaction.Transactional;
+import org.deliverygo.MainApplication;
+import org.deliverygo.login.dto.SignUpRequest;
+import org.deliverygo.login.entity.User;
+import org.deliverygo.login.repository.UserRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.test.context.ActiveProfiles;
+
+import static org.deliverygo.login.constants.UserGrade.OWNER;
+
+@ActiveProfiles("test")
+@SpringBootTest(classes = MainApplication.class)
+@Transactional
+class SignUpServiceImplTest {
+
+    @Autowired
+    private SignUpServiceImpl  signUpService;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Test
+    @DisplayName("회원이 없으면 회원가입에 성공한다.")
+    void userIsNotExistThenSignUpSuccess() {
+        //given
+        SignUpRequest signUpRequest = new SignUpRequest("js.min@google.com",
+                "11112222",
+                "민장식",
+                "01020893750",
+                "인천 서구 당하동",
+                OWNER);
+        //when
+        signUpService.signUp(signUpRequest);
+
+        //then
+        User findUser = userRepository.findByEmail(signUpRequest.email()).orElseThrow();
+
+        Assertions.assertEquals(signUpRequest.email(), findUser.getEmail());
+    }
+}
