f25bac84bce4359f996b770866f07b0080810865
js.min <js.min@hansol.com>
2025-03-02 21:27:48 +0900

    refactor: @EqualsAndHashCode 사용

--- module-auth/src/main/java/org/deliverygo/login/entity/User.java
+++ module-auth/src/main/java/org/deliverygo/login/entity/User.java
@@ -2,16 +2,16 @@ package org.deliverygo.login.entity;
 
 import jakarta.persistence.*;
 import lombok.AccessLevel;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.deliverygo.login.constants.UserGrade;
 import org.deliverygo.login.dto.SignUpRequest;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-import java.util.Objects;
-
 @Entity
 @Getter
+@EqualsAndHashCode(callSuper = true)
 @Table(name = "users")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class User extends BaseEntity {
@@ -56,23 +56,5 @@ public class User extends BaseEntity {
                 signUpRequest.address(),
                 signUpRequest.phone(),
                 signUpRequest.grade());
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (o == null || getClass() != o.getClass()) return false;
-        User user = (User) o;
-        return Objects.equals(getPhone(), user.getPhone()) &&
-                Objects.equals(getId(), user.getId()) &&
-                Objects.equals(getPassword(), user.getPassword()) &&
-                Objects.equals(getName(), user.getName()) &&
-                Objects.equals(getEmail(), user.getEmail()) &&
-                Objects.equals(getAddress(), user.getAddress()) &&
-                getGrade() == user.getGrade();
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(getId(), getPassword(), getName(), getEmail(), getAddress(), getPhone(), getGrade());
     }
 }
