06d7f4089a9635016582bfea300a448e5db181a5
js.min <js.min@hansol.com>
2025-03-08 10:48:15 +0900

    update: insertJwt 네이밍 변경

--- module-auth/src/main/java/org/deliverygo/login/repository/JwtRepository.java
+++ module-auth/src/main/java/org/deliverygo/login/repository/JwtRepository.java
@@ -1,6 +1,18 @@
 package org.deliverygo.login.repository;
 
-public interface JwtRepository {
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Repository;
 
-    void insert(String key, String jwt, long expire);
+import static org.deliverygo.login.domain.JwtToken.KEY_PREFIX;
+
+@Repository
+@RequiredArgsConstructor
+public class JwtRepository {
+
+    private final RedisTemplate<String, Object> redisTemplate;
+
+    public void insertJwt(String key, String jwt, long expire) {
+        redisTemplate.opsForValue().set(KEY_PREFIX + ":" + key, jwt, expire);
+    }
 }
