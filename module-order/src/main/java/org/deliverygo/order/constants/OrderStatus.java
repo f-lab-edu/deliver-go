4cedfde033982932193df52c09a7e520e6d80bd8
민장식 선임 <js.min@hansol.com>
2025-03-24 17:50:16 +0900

    update: orderStatus 상태 전이 적용

--- module-order/src/main/java/org/deliverygo/order/constants/OrderStatus.java
+++ module-order/src/main/java/org/deliverygo/order/constants/OrderStatus.java
@@ -1,10 +1,23 @@
 package org.deliverygo.order.constants;
 
+import java.util.EnumSet;
+import java.util.Set;
+
 public enum OrderStatus {
 
-    RECEIVED,
-    SUCCESS,
-    FAILED,
-    CANCEL_REQUEST,
-    CANCELED
+    SUCCESS(EnumSet.noneOf(OrderStatus.class)),
+    FAILED(EnumSet.noneOf(OrderStatus.class)),
+    CANCELED(EnumSet.noneOf(OrderStatus.class)),
+    CANCEL_REQUEST(EnumSet.of(CANCELED)),
+    RECEIVED(EnumSet.of(SUCCESS, FAILED, CANCEL_REQUEST));
+
+    private final Set<OrderStatus> nextStates;
+
+    OrderStatus(Set<OrderStatus> nextStates) {
+        this.nextStates = nextStates;
+    }
+
+    public boolean canTransitionTo(OrderStatus next) {
+        return nextStates.contains(next);
+    }
 }
