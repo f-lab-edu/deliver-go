938176aa0c0606091e263c64cabe4deeee9c7f38
js.min <js.min@hansol.com>
2025-04-16 00:11:22 +0900

    fix: google client 수정

--- module-delivery/src/main/java/org/deliverygo/delivery/controller/DeliveryController.java
+++ module-delivery/src/main/java/org/deliverygo/delivery/controller/DeliveryController.java
@@ -3,20 +3,20 @@ package org.deliverygo.delivery.controller;
 import lombok.RequiredArgsConstructor;
 import org.deliverygo.delivery.dto.SaveDeliveryLocationRequest;
 import org.deliverygo.delivery.service.DeliveryService;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("api/v1")
+@RestController
+@RequestMapping("/api/v1")
 @RequiredArgsConstructor
 public class DeliveryController {
 
     private final DeliveryService deliveryService;
 
-
     @PostMapping("/riders/{riderId}/location")
-    public void saveRiderLocation(SaveDeliveryLocationRequest request) {
+    public void saveRiderLocation(@RequestBody SaveDeliveryLocationRequest request) {
         deliveryService.saveDeliveryLocation(request);
     }
 }
